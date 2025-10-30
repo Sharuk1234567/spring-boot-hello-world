8c5499db7eae4764a4427fb2d8dc0aca0a4efd9f
Syed Rizwan <syedrizwan93814@gmail.com>
2025-10-30 16:28:56 +0530

    commit-4

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -8,7 +8,7 @@ public class HelloWorldController {
 
     @GetMapping("/hello")
     public String sendGreetings() {
-         return "Hello, World! testing the code project2, monitoring on grafana, Azure-DevOps working on new project, new task in-progress - 30/10/2025";
+         return "Hello, World! testing the code project2, monitoring on grafana, Azure-DevOps working on new project, new task in-progress - 30/10/2025 4:28 pm";
 
     }
 }
